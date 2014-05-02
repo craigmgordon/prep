81e92b4135813806e715f0dd86abd8e3a207c013
Craig Gordon <craigmgordon@gmail.com>
2014-05-02 10:11:03 +0100

    new customer class

---
+++ src/main/java/prep/Customer.java
@@ -0,0 +1,28 @@
+package prep;
+
+public class Customer {
+	private String foreame;
+	private String surname;
+	private String customerRef;
+	public String getForeame() {
+		return foreame;
+	}
+	public void setForeame(String foreame) {
+		this.foreame = foreame;
+	}
+	public String getSurname() {
+		return surname;
+	}
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+	public String getCustomerRef() {
+		return customerRef;
+	}
+	public void setCustomerRef(String customerRef) {
+		this.customerRef = customerRef;
+	}
+	
+	
+	
+}
